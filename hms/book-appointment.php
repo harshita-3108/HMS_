13e5206f0bcb7b182a393f592b4fcfbecb34ee75
harshita-3108 <mharshita31@gmail.com>
2022-06-16 01:54:55 +0530

    added payment_module

--- hms/book-appointment.php
+++ hms/book-appointment.php
@@ -18,16 +18,11 @@ $_SESSION['userid'] = $userid;
 $_SESSION['appointmentDate'] = $appdate;
 
 $query=mysqli_query($con,"insert into appointment(doctorSpecialization,doctorId,userId,consultancyFees,appointmentDate,appointmentTime,userStatus,doctorStatus) values('$specilization','$doctorid','$userid','$fees','$appdate','$time','$userstatus','$docstatus')");
-$extra="makePayment.php";//
+$extra="../paymentModule/index.php";//
 $host=$_SERVER['HTTP_HOST'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http://$host$uri/$extra");	
-// if($query)
-	// {
-	// 	echo "<script>alert('Your appointment successfully booked');</script>";
-	// }
-
 
 }
 ?>
